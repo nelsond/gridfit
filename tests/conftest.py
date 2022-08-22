b885550dd0c1c75c5d92344802c771144c2e8516
Nelson Darkwah Oppong <n@darkwahoppong.com>
2022-08-22 14:42:08 +0200

    Add explicit type and value checks for arguments of rect.find_dominant_angle

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
-import sys
 import os
-
 import pytest
-
-current_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(current_path, '..'))
 
 
 @pytest.fixture
--- tests/rect/test_find_dominant_angle.py
+++ tests/rect/test_find_dominant_angle.py
@@ -21,3 +21,30 @@ def test_find_dominant_angle_returns_expected_result(load_fixture_data):  # noqa
 def test_find_dominant_angle_accepts_debug_flag(matplotlib_figure):
     data = np.zeros((10, 10))
     find_dominant_angle(data, debug=True)
+
+
+def test_find_dominant_angle_raises_value_error_for_invalid_data_type():
+    for invalid_value in (10, 10., (10, 10.), [100, 100]):
+        with pytest.raises(ValueError):
+            find_dominant_angle(invalid_value)
+
+
+def test_find_dominant_angle_raises_value_error_for_invalid_data_shape():
+    with pytest.raises(ValueError):
+        find_dominant_angle(np.zeros((10, 10, 10)))
+
+
+def test_find_dominant_angle_raises_value_error_for_invalid_angular_range_type():
+    for invalid_value in (10, [10, 10]):
+        with pytest.raises(ValueError):
+            find_dominant_angle(np.zeros((10, 10)), angular_range=invalid_value)
+
+    
+def test_find_dominant_angle_raises_value_error_for_invalid_angular_range_length():
+    with pytest.raises(ValueError):
+        find_dominant_angle(np.zeros((10, 10)), angular_range=(10, 10, 10))
+
+
+def test_find_dominant_angle_raises_value_error_for_invalid_angular_range_values():
+    with pytest.raises(ValueError):
+        find_dominant_angle(np.zeros((10, 10)), angular_range=(10, 10))
